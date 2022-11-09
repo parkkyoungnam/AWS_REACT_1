7f48fcc21d955ce51528fe81c16dfa3acba4fd02
parkkyoungnam <pkn0813@naver.com>
2022-11-09 13:58:15 +0900

    리액트_aws

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import Board from './Board';
+import {useState} from 'react'
+import Button from './Button'
+import './App.css'
+
+function random(n)
+{
+    return Math.ceil(Math.random() * n);
+}
+
+function App()
+{
+
+    const [myHistory, setMyHistory] = useState([]);
+
+
+    const [otherHistory, setOtherHistory] = useState([]);
+
+    const handleRollClick = () => {
+        const nextMyNum = random(6);
+        const nextOtherNum = random(6);
+
+        setMyHistory([...myHistory, nextMyNum]);
+        setOtherHistory([...otherHistory, nextOtherNum]);
+    }
+
+    const handleClearClick = () => {
+
+        setMyHistory([]);
+        setOtherHistory([]);
+    }
+
+    return (
+        
+        <div className='App'>
+            <div>
+                <Button className='App-button' color = "blue" onClick={handleRollClick}>던지기</Button>
+                <Button className='App-button' color = "red" onClick={handleClearClick}>처음으로</Button>
+            </div>
+            <div>
+                <Board name="나" color="blue"  gameHistory={myHistory}/>
+                <Board name="상대" color="red" gameHistory={otherHistory}/>
+            </div>
+
+        </div>
+
+    )   
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React, { Fragment } from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const test = "pkn"
+
+root.render(
+
+  <Fragment>
+
+    <App></App>
+
+  </Fragment>
+
+
+  
+
+);
